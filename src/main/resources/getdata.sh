b5b79812c702daf9650a04de6d994a7595e248a3
Malte Rohde <malte.rohde@inf.fu-berlin.de>
2011-07-07 12:14:14 +0200

    add additional files to getdata.sh

--- src/main/resources/getdata.sh
+++ src/main/resources/getdata.sh
@@ -1,15 +1,34 @@
 #!/bin/sh
 
-BASEURL="ftp://ftp.fu-berlin.de/pub/misc/movies/database/"
-FILES="actors.list actresses.list directors.list locations.list release-dates.list plot.list"
+IMDBURL="ftp://ftp.fu-berlin.de/pub/misc/movies/database/"
+IMDBFILES="actors.list actresses.list directors.list locations.list release-dates.list plot.list"
+ADDITIONAL_URL="http://student:dbms@www.inf.fu-berlin.de/lehre/SS11/DBS-Intro/"
+ADDITIONAL_FILES="modmovies.list customers.list rentals.list"
+
 PROCESSOR=wget
 
-for i in $FILES
+for i in $IMDBFILES
 do
+	echo -n "Downloading $i... "
 	if [ ! -e $i ]
 	then
-		$PROCESSOR $BASEURL$i.gz
+		echo
+		$PROCESSOR $IMDBURL$i.gz
 		gzip -d $i.gz
+	else
+		echo "already there."
+	fi
+done
+
+for j in $ADDITIONAL_FILES
+do
+	echo -n "Downloading $j... "
+	if [ ! -e $j ]
+	then
+		echo
+		$PROCESSOR $ADDITIONAL_URL$j
+	else
+		echo "already there."
 	fi
 done
 
