67f6dfa77c6b9d7c4dc44f0c2fd2e699e771e432
Malte Rohde <malte.rohde@gmx.de>
2011-07-12 01:18:23 +0200

    [QueryB] Rewrite with pl/pgsql (UNFINISHED)
    pl/pgsql sucks. Does not work, don't know why. Run it.

--- src/main/resources/sql/queryb.sql
+++ src/main/resources/sql/queryb.sql
@@ -1,29 +1,85 @@
--- Wir nehmen Januar 2011
+-- commented out as postgres complains if already created and there's
+-- no way of CREATing it IF NOT EXISTs...
+-- CREATE LANGUAGE plpgsql;
 
-SELECT cus_id, SUM(summe) AS total FROM (
+-- this method should move to create.sql someday
+CREATE OR REPLACE FUNCTION totalcharges(integer, integer, integer)
+RETURNS integer
+AS
+$$
+	DECLARE
+		rental RECORD;
+		result NUMERIC;
+		flat boolean;
+		firstB RECORD;
+		firstBEver timestamp;
+		sqlexpr text;
+		pcus_id ALIAS FOR $1;
+		pmonth ALIAS FOR $2;
+		pyear ALIAS FOR $3;
+	BEGIN
+			/* TODO s/560'/' || CAST(pcus_id AS text)/ */
+			sqlexpr := 'SELECT price_category, type, startdate, duration
+					FROM rentals JOIN movies
+					ON rentals.mov_id = movies.mov_id
+					WHERE rentals.cus_id = 560';
+			flat := false;
+			FOR rental IN EXECUTE sqlexpr LOOP
+				IF rental.type = 'flat' THEN
+					flat := true;
+					IF firstB IS NULL AND rental.price_category = 'B' THEN
+						firstB = rental;
+					END IF;
+					result := result + rental.duration * 0.19;	
+				ELSE 
+					IF rental.type = 'speedy' THEN
+						IF rental.price_category = 'A' THEN
+							result := result + rental.duration * 0.19;
+						ELSE
+							result := result + rental.duration * 0.15;
+						END IF;
+					ELSE
+						IF rental.price_category = 'A' THEN
+							result := result + rental.duration * 1.29;
+						ELSE
+							result := result + rental.duration * 0.79;
+						END IF;
+					END IF;
+				END IF;
+			END LOOP;
+			
+			IF flat = true THEN
+				result := result + 10;
+				IF firstB <> NULL THEN
+					SELECT MIN(startdate) INTO firstBEver 
+					FROM rentals
+					WHERE cus_id = pcus_id;
+					
+					IF firstB.starttime = firstBEver THEN
+						result := result - firstB.duration * 0.19;
+					END IF;
+				END IF;
+			END IF;
+			
+			RETURN result;
+	END;
+$$
+LANGUAGE 'plpgsql' STABLE;
 
--- speedy, category A = 0.19/h
-(SELECT cus_id, SUM(duration * 0.19) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'speedy' AND price_category = 'A'  AND EXTRACT(MONTH FROM startdate) = 1 AND EXTRACT(YEAR FROM startdate) = 2011 GROUP BY cus_id)
-UNION
--- speedy, category B = 0.15/h
-(SELECT cus_id, SUM(duration * 0.15) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'speedy' AND price_category = 'B'  AND EXTRACT(MONTH FROM startdate) = 1 AND EXTRACT(YEAR FROM startdate) = 2011 GROUP BY cus_id)
-UNION
--- starter, category A = 1.29/d
-(SELECT cus_id, SUM(duration * 1.29) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'starter' AND price_category = 'A'  AND EXTRACT(MONTH FROM startdate) = 1 AND EXTRACT(YEAR FROM startdate) = 2011 GROUP BY cus_id)
-UNION
--- starter, category B = 0.79/d
-(SELECT cus_id, SUM(duration * 0.79) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'starter' AND price_category = 'B'  AND EXTRACT(MONTH FROM startdate) = 1 AND EXTRACT(YEAR FROM startdate) = 2011 GROUP BY cus_id)
-UNION
--- flat, category A = 0.19/d
-(SELECT cus_id, SUM(duration * 0.19) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'flat' AND price_category = 'A' AND  EXTRACT(MONTH FROM startdate) = 1 AND EXTRACT(YEAR FROM startdate) = 2011 GROUP BY cus_id)
-UNION
--- flat, category B = 0.19/d
-(SELECT cus_id, (SUM(duration * 0.19)) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'flat' AND price_category = 'B' AND EXTRACT(MONTH FROM startdate) = 1 AND EXTRACT(YEAR FROM startdate) = 2011 GROUP BY cus_id)
-UNION
--- flat, erster Film der category B gratis, d.h. falls der erste Film in den Monat Januar gefallen ist
-(SELECT r1.cus_id AS cus_id, (SUM(duration * -0.19)) AS summe FROM rentals r1 JOIN movies m ON r1.mov_id = m.mov_id WHERE type = 'flat' AND price_category = 'B' AND EXTRACT(MONTH FROM startdate) = 1 AND EXTRACT(YEAR FROM startdate) = 2011 AND startdate IN (SELECT MIN(startdate) FROM rentals r2 WHERE r1.cus_id = r2.cus_id GROUP BY r2.cus_id)) GROUP BY r1.cus_id)
-UNION
--- flat, 10.-/month
-(SELECT cus_id, 10 AS summe FROM rentals WHERE type = 'flat' AND EXTRACT(MONTH FROM startdate) = 1 AND EXTRACT(YEAR FROM startdate) = 2011 GROUP BY cus_id)
 
-) AS tbl GROUP BY cus_id;
+CREATE OR REPLACE FUNCTION getjanuaryreports()
+RETURNS TABLE(cus_id integer, summe integer)
+AS
+$$
+	DECLARE
+		result record;
+	BEGIN
+		FOR result IN SELECT cus_id, totalcharges(cus_id, 1, 2011) FROM rentals LOOP
+			RETURN NEXT;
+		END LOOP;
+		RETURN;
+	END;
+$$
+LANGUAGE 'plpgsql' STABLE;
+
+SELECT * FROM getjanuaryreports();
