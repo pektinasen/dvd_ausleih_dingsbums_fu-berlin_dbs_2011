8850f5f5a4b734a9f361bcbbf3f7b1299de626ef
Malte Rohde <malte.rohde@gmx.de>
2011-07-11 22:41:17 +0200

    [FIX] Query B: first B movie for free, restrict year to 2011
    Currently, customers don't pay for the first day of their first
    B rental in january. As required by the FAQ, flat customers should
    not pay for their first B movie at all (no matter how long they have
    it). Besides, my implementation used the first B movie in January,
    where it should only include B rentals which are literally the first
    B rental ever.
    
    Das Ganze koennte refactored werden, dringend. Kann man irgendwie temporaere
    Tabellen anlegen?

--- src/main/resources/sql/queryb.sql
+++ src/main/resources/sql/queryb.sql
@@ -1,24 +1,29 @@
+-- Wir nehmen Januar 2011
+
 SELECT cus_id, SUM(summe) AS total FROM (
 
 -- speedy, category A = 0.19/h
-(SELECT cus_id, SUM(duration * 0.19) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'speedy' AND price_category = 'A'  AND EXTRACT(MONTH FROM startdate) = 1 GROUP BY cus_id)
+(SELECT cus_id, SUM(duration * 0.19) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'speedy' AND price_category = 'A'  AND EXTRACT(MONTH FROM startdate) = 1 AND EXTRACT(YEAR FROM startdate) = 2011 GROUP BY cus_id)
 UNION
 -- speedy, category B = 0.15/h
-(SELECT cus_id, SUM(duration * 0.15) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'speedy' AND price_category = 'B'  AND EXTRACT(MONTH FROM startdate) = 1 GROUP BY cus_id)
+(SELECT cus_id, SUM(duration * 0.15) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'speedy' AND price_category = 'B'  AND EXTRACT(MONTH FROM startdate) = 1 AND EXTRACT(YEAR FROM startdate) = 2011 GROUP BY cus_id)
 UNION
 -- starter, category A = 1.29/d
-(SELECT cus_id, SUM(duration * 1.29) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'starter' AND price_category = 'A'  AND EXTRACT(MONTH FROM startdate) = 1 GROUP BY cus_id)
+(SELECT cus_id, SUM(duration * 1.29) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'starter' AND price_category = 'A'  AND EXTRACT(MONTH FROM startdate) = 1 AND EXTRACT(YEAR FROM startdate) = 2011 GROUP BY cus_id)
 UNION
 -- starter, category B = 0.79/d
-(SELECT cus_id, SUM(duration * 0.79) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'starter' AND price_category = 'B'  AND EXTRACT(MONTH FROM startdate) = 1 GROUP BY cus_id)
+(SELECT cus_id, SUM(duration * 0.79) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'starter' AND price_category = 'B'  AND EXTRACT(MONTH FROM startdate) = 1 AND EXTRACT(YEAR FROM startdate) = 2011 GROUP BY cus_id)
 UNION
 -- flat, category A = 0.19/d
-(SELECT cus_id, SUM(duration * 0.19) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'flat' AND price_category = 'A' AND  EXTRACT(MONTH FROM startdate) = 1 GROUP BY cus_id)
+(SELECT cus_id, SUM(duration * 0.19) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'flat' AND price_category = 'A' AND  EXTRACT(MONTH FROM startdate) = 1 AND EXTRACT(YEAR FROM startdate) = 2011 GROUP BY cus_id)
 UNION
--- flat, category B = 0.19/d + der erste Film gratis (-0.19)
-(SELECT cus_id, (SUM(duration * 0.19) - 0.19) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'flat' AND price_category = 'B' AND EXTRACT(MONTH FROM startdate) = 1 GROUP BY cus_id)
-UNION 
+-- flat, category B = 0.19/d
+(SELECT cus_id, (SUM(duration * 0.19)) AS summe FROM rentals JOIN movies ON rentals.mov_id = movies.mov_id WHERE type = 'flat' AND price_category = 'B' AND EXTRACT(MONTH FROM startdate) = 1 AND EXTRACT(YEAR FROM startdate) = 2011 GROUP BY cus_id)
+UNION
+-- flat, erster Film der category B gratis, d.h. falls der erste Film in den Monat Januar gefallen ist
+(SELECT r1.cus_id AS cus_id, (SUM(duration * -0.19)) AS summe FROM rentals r1 JOIN movies m ON r1.mov_id = m.mov_id WHERE type = 'flat' AND price_category = 'B' AND EXTRACT(MONTH FROM startdate) = 1 AND EXTRACT(YEAR FROM startdate) = 2011 AND startdate IN (SELECT MIN(startdate) FROM rentals r2 WHERE r1.cus_id = r2.cus_id GROUP BY r2.cus_id)) GROUP BY r1.cus_id)
+UNION
 -- flat, 10.-/month
-(SELECT cus_id, 10 AS summe FROM rentals WHERE type = 'flat' AND EXTRACT(MONTH FROM startdate) = 1 GROUP BY cus_id)
+(SELECT cus_id, 10 AS summe FROM rentals WHERE type = 'flat' AND EXTRACT(MONTH FROM startdate) = 1 AND EXTRACT(YEAR FROM startdate) = 2011 GROUP BY cus_id)
 
 ) AS tbl GROUP BY cus_id;
