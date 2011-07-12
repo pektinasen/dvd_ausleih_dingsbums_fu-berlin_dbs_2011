dc0095efd1ebe104d3fe1809b186bcfd35d5aff9
Malte Rohde <malte.rohde@inf.fu-berlin.de>
2011-07-12 21:29:21 +0200

    started working on shortest path algo

--- src/main/java/de/softwarekollektiv/dbs/queries/complex/QueryF.java
+++ src/main/java/de/softwarekollektiv/dbs/queries/complex/QueryF.java
@@ -1,6 +1,9 @@
 package de.softwarekollektiv.dbs.queries.complex;
 
 import java.io.PrintStream;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import de.softwarekollektiv.dbs.app.MenuItem;
 import de.softwarekollektiv.dbs.dbcon.DbConnection;
@@ -9,6 +12,9 @@ class QueryF implements MenuItem {
 
 	private final PrintStream out;
 	private final DbConnection dbcon;
+	
+	private PreparedStatement actIdStmt;
+	private PreparedStatement hasWorkedWithStmt;
 	
 	QueryF(PrintStream out, DbConnection dbcon) {
 		this.out = out;
@@ -27,8 +33,45 @@ class QueryF implements MenuItem {
 
 	@Override
 	public boolean run() throws Exception {
-		// TODO Auto-generated method stub
+		
+		String[][] pairs = {
+				{"Depp, Johnny", "Dalton, Timothy"},
+				{"Depp, Johnny", "Diehl, August"},
+				{"Murray, Bill", "Stallone, Sylvester"},
+				{"Norton, Edward", "Cheadle, Don"}
+		};
+		actIdStmt = dbcon.getConnection().prepareStatement("SELECT act_id FROM actors WHERE name = ?");
+		hasWorkedWithStmt = dbcon.getConnection().prepareStatement(
+				"SELECT act_id FROM features WHERE mov_id IN (SELECT mov_id FROM features WHERE act_id = ?)");
+		
+		out.println();
+		for(String[] pair : pairs) {
+			int startId = getId(pair[0]);
+			int endId = getId(pair[1]);
+			out.println("Shortest path between " + pair[0] + " and " + pair[1] + " is: " + shortestPath(startId, endId));
+		}
+		
+		actIdStmt.close();
+		hasWorkedWithStmt.close();
 		return true;
 	}
-
+	
+	/**
+	 * BFS
+	 */
+	private int shortestPath(int startId, int endId) {
+		return 0;
+	}
+	
+	private List<Integer> getActorsWhoWorkedWith(int id) throws SQLException {
+		return null;
+	}
+	
+	private int getId(String name) throws SQLException {
+		actIdStmt.setString(1, name);
+		ResultSet rs = actIdStmt.executeQuery();
+		// must exist
+		rs.next();
+		return rs.getInt(1);
+	}
 }
