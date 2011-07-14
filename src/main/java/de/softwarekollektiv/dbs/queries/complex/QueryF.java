27cb03665da100cb81e315ef4b40e129393e6007
Malte Rohde <malte.rohde@gmx.de>
2011-07-14 23:48:57 +0200

    Remove some unused imports and fix some warnings

--- src/main/java/de/softwarekollektiv/dbs/queries/complex/QueryF.java
+++ src/main/java/de/softwarekollektiv/dbs/queries/complex/QueryF.java
@@ -9,9 +9,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
-import java.util.Set;
 import java.util.TreeMap;
-import java.util.TreeSet;
 
 import de.softwarekollektiv.dbs.app.MenuItem;
 import de.softwarekollektiv.dbs.dbcon.DbConnection;
